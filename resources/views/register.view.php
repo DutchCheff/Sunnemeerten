fdc280bc51918c69af95b8610d46a9a6805b841f
fernando bos <fdmbos@st.noorderpoort.nl>
2025-04-04 13:21:57 +0200

    register page start

---
+++ resources/views/register.view.php
@@ -0,0 +1,32 @@
+<?php require_once '../resources/views/header.view.php'; ?>
+<div class="w-full h-screen bg-background absolute">
+    <div class="bg-card mx-7 mt-28 rounded-2xl text-text px-3 py-4">
+        <h2 class="text-2xl font-semibold text-center mb-4">Registreren</h2>
+        <form>
+            <div class="mb-4">
+                <label class="block">Naam</label>
+                <input type="text" class="w-full mt-1 p-2 border-black rounded-lg bg-[#1F2937] text-white focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="mb-4">
+                <label class="block">Email</label>
+                <input type="email" class="w-full mt-1 p-2 border-black rounded-lg bg-[#1F2937] text-white focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="mb-4">
+                <label class="block">Wachtwoord</label>
+                <input type="password" class="w-full mt-1 p-2 border-black rounded-lg bg-[#1F2937] text-white focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="mb-6">
+                <label class="block">Herhaal wachtwoord</label>
+                <input type="password" class="w-full mt-1 p-2 border-black rounded-lg bg-[#1F2937] text-white focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="flex justify-between">
+                <button type="button" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-400">voorwarden</button>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Sign Up</button>
+            </div>
+        </form>
+    </div>
+</div>

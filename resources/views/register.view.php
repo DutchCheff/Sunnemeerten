db18f589ef203ddf3a8aed0eeb5c9063418602e2
Wiebe Geus <wiebegeus@gmail.com>
2025-04-07 09:38:31 +0200

    registreer knoppen juiste kleur gegeven

--- resources/views/register.view.php
+++ resources/views/register.view.php
@@ -24,8 +24,8 @@
             </div>
 
             <div class="flex justify-between">
-                <button type="button" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-400">voorwaarden</button>
-                <button type="submit" class="bg-blue-500 text-text px-4 py-2 rounded-lg hover:bg-blue-600">Sign Up</button>
+                <button type="button" class="bg-accent2 text-text px-4 py-2 rounded-lg hover:bg-gray-400">Voorwaarden</button>
+                <button type="submit" class="bg-accent1 text-text px-4 py-2 rounded-lg hover:bg-blue-600">Sign Up</button>
             </div>
         </form>
     </div>

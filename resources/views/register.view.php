a7ed7df731cfe5f2073daa7dc9ffc7403bf35e2b
Wiebe Geus <wiebegeus@gmail.com>
2025-04-07 13:04:38 +0200

    login pagina

--- resources/views/register.view.php
+++ resources/views/register.view.php
@@ -1,7 +1,7 @@
 <?php require_once '../resources/views/header.view.php'; ?>
 <div class="w-full h-screen bg-background absolute">
     <div class="bg-card mx-7 mt-28 rounded-2xl text-text px-3 py-4">
-        <h2 class="text-2xl font-semibold text-center mb-4">Registreren</h2>
+        <h2 class="text-4xl font-semibold text-center mb-4">Registreren</h2>
         <form>
             <div class="mb-4">
                 <label class="block">Naam</label>
@@ -30,3 +30,4 @@
         </form>
     </div>
 </div>
+<?php require_once '../resources/views/footer.view.php'; ?>

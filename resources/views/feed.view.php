114b1c59c6526a6045f7cad6e9af05999f9115d2
fernando bos <fdmbos@st.noorderpoort.nl>
2025-04-08 11:26:08 +0200

    yes

---
+++ resources/views/feed.view.php
@@ -0,0 +1,4 @@
+<?php require_once '../resources/views/header.view.php'; ?>
+<div class="bg-card mx-7 mt-28 rounded-2xl text-text px-3 py-4">
+</div>
+<?php require_once '../resources/views/footer.view.php'; ?>

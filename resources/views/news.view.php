427db2e59a806163418c515e4ba43f437fe962d5
Laurens <laurens@laurensk.nl>
2025-04-09 12:03:32 +0200

    test

--- resources/views/news.view.php
+++ resources/views/news.view.php
@@ -6,12 +6,18 @@
     <div class="mt-14">
         <img src="/images/logo1.png" alt="" class="scale-90">
     </div>
-    <h1 class="text-4xl texta-center">Nieuws pagina</h1>
+    <h1 class="text-4xl texta-center text-white text-center">Nieuws pagina</h1>
 
-    <div class="bg-card mx-7 h-[40rem] rounded-2xl text-text px-3 py-4">
+    <div class="bg-card mx-7 rounded-2xl text-text px-3 py-4">
         <div>
-        </div>
-        
+            <h1 class="text-4xl texta-center">Nieuws</h1>
+            <p> Hier komt het nieuws van de parade te staan. Dit kan gaan over de route, de tijdstippen en andere belangrijke informatie.</p>           
+    </div>
+</div>
+<div class="bg-accent1 mx-7 rounded-2xl text-text px-3 py-4 mt-4">
+        <div>
+            <h1 class="text-4xl texta-center">Nieuws</h1>
+            <p> Hier komt het nieuws van de parade te staan. Dit kan gaan over de route, de tijdstippen en andere belangrijke informatie.</p>           
     </div>
 </div>
 <?php require_once '../resources/views/footer.view.php'; ?>
